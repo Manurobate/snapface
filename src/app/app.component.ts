0f2d4e090f09a90e4afafe0bbc55c30004b72a94
Manurobate <manurobate@msn.com>
2024-02-05 21:47:48 +0100

    Add header and create list of snapfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -7,33 +6,8 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.faceSnaps = [
-      {
-        title: "Archibald",
-        description: "Mon meilleur ami depuis tout petit",
-        imageUrl: "https://m.media-amazon.com/images/I/71GPmk+2X4L.jpg",
-        createdDate: new Date(),
-        snaps: 50
-      },
-      {
-        title: "Steven",
-        description: "Mon idole",
-        imageUrl: "https://d3j2s6hdd6a7rg.cloudfront.net/v2/uploads/media/default/0001/31/thumb_30605_default_news_size_5.jpeg",
-        createdDate: new Date(),
-        snaps: 194,
-        location: "Liverpool"
-      },
-      {
-        title: "Paziols",
-        description: "Lieu de rêve",
-        imageUrl: "https://www.hit-occitanie.com/media/hit/ois/ASCLAR011V50L3KA/images/PETROLHEAD3.jpg",
-        createdDate: new Date(),
-        snaps: 141,
-        location: "Paziols"
-      }
-    ]
+
   }
 }
---
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Component({
+  selector: 'app-face-snap-list',
+  templateUrl: './face-snap-list.component.html',
+  styleUrl: './face-snap-list.component.scss'
+})
+export class FaceSnapListComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
+
+  ngOnInit(): void {
+    this.faceSnaps = [
+      {
+        title: "Archibald",
+        description: "Mon meilleur ami depuis tout petit",
+        imageUrl: "https://m.media-amazon.com/images/I/71GPmk+2X4L.jpg",
+        createdDate: new Date(),
+        snaps: 50
+      },
+      {
+        title: "Steven",
+        description: "Mon idole",
+        imageUrl: "https://d3j2s6hdd6a7rg.cloudfront.net/v2/uploads/media/default/0001/31/thumb_30605_default_news_size_5.jpeg",
+        createdDate: new Date(),
+        snaps: 194,
+        location: "Liverpool"
+      },
+      {
+        title: "Paziols",
+        description: "Lieu de rêve",
+        imageUrl: "https://www.hit-occitanie.com/media/hit/ois/ASCLAR011V50L3KA/images/PETROLHEAD3.jpg",
+        createdDate: new Date(),
+        snaps: 141,
+        location: "Paziols"
+      }
+    ]
+  }
+}
