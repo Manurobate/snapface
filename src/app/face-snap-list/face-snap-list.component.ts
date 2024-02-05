8f52840c5d3e6b78648f8ba10cda138662992e55
Manurobate <manurobate@msn.com>
2024-02-05 22:24:30 +0100

    use service to get all facesnaps

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FaceSnapsService } from '../services/face-snaps.service';
 import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
@@ -7,33 +8,12 @@ import { FaceSnap } from '../models/face-snap.model';
   styleUrl: './face-snap-list.component.scss'
 })
 export class FaceSnapListComponent implements OnInit {
+
+  constructor(private faceSnapsService: FaceSnapsService) { }
+
   faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.faceSnaps = [
-      {
-        title: "Archibald",
-        description: "Mon meilleur ami depuis tout petit",
-        imageUrl: "https://m.media-amazon.com/images/I/71GPmk+2X4L.jpg",
-        createdDate: new Date(),
-        snaps: 50
-      },
-      {
-        title: "Steven",
-        description: "Mon idole",
-        imageUrl: "https://d3j2s6hdd6a7rg.cloudfront.net/v2/uploads/media/default/0001/31/thumb_30605_default_news_size_5.jpeg",
-        createdDate: new Date(),
-        snaps: 194,
-        location: "Liverpool"
-      },
-      {
-        title: "Paziols",
-        description: "Lieu de rêve",
-        imageUrl: "https://www.hit-occitanie.com/media/hit/ois/ASCLAR011V50L3KA/images/PETROLHEAD3.jpg",
-        createdDate: new Date(),
-        snaps: 141,
-        location: "Paziols"
-      }
-    ]
+    this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
   }
 }
