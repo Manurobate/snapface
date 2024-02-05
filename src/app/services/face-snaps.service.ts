2e8652c2c0778f638d70645223dbcb077caad70d
Manurobate <manurobate@msn.com>
2024-02-05 22:30:27 +0100

    Add facesnap ID

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -8,6 +8,7 @@ import { FaceSnap } from "../models/face-snap.model";
 export class FaceSnapsService {
     private faceSnaps: FaceSnap[] = [
         {
+            id: 1,
             title: "Archibald",
             description: "Mon meilleur ami depuis tout petit",
             imageUrl: "https://m.media-amazon.com/images/I/71GPmk+2X4L.jpg",
@@ -15,6 +16,7 @@ export class FaceSnapsService {
             snaps: 50
         },
         {
+            id: 2,
             title: "Steven",
             description: "Mon idole",
             imageUrl: "https://d3j2s6hdd6a7rg.cloudfront.net/v2/uploads/media/default/0001/31/thumb_30605_default_news_size_5.jpeg",
@@ -23,6 +25,7 @@ export class FaceSnapsService {
             location: "Liverpool"
         },
         {
+            id: 3,
             title: "Paziols",
             description: "Lieu de rêve",
             imageUrl: "https://www.hit-occitanie.com/media/hit/ois/ASCLAR011V50L3KA/images/PETROLHEAD3.jpg",
